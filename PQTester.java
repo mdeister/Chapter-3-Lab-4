12f4513cb8f182e8a6dc72933cecc97b75bd7db9
mdeister <Mjdeister@fjuhsd.org>
2021-04-21 22:17:59 -0700

    Add files via upload

---
+++ PQTester.java
@@ -0,0 +1,40 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -  
+
+import java.util.Queue;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class PQTester
+{
+ private Queue<String> pQueue;
+
+ public PQTester()
+ {
+ }
+
+ public PQTester(String list)
+ {
+ }
+
+ public void setPQ(String list)
+ {
+ }
+ 
+ public Object getMin()
+ {
+  return "";
+ }
+ 
+ public String getNaturalOrder()
+ {
+  String output="";
+  return output;  
+ }
+
+ //write a toString method
+}
